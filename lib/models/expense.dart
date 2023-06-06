e80adbd4ae2d875c8b1ff9924f29b1324cea9a80
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-06 15:44:28 -0500

    feat: use enum to represent expense categories

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,24 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum ExpenseCategory {
+  food,
+  travel,
+  leisure,
+  work,
+}
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }); // : id = uuid.v4(); // initializer list
 
   final String id = uuid.v4(); // not required if used the initalizer list
   final String title;
   final double amount;
   final DateTime date;
+  final ExpenseCategory category;
 }
