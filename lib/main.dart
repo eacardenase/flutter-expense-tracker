ef67285ad59b16b1947f4ececd889012110e808c
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-07 19:58:52 -0500

    feat: setting base theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  // seedColor: const Color.fromARGB(
+  //   255,
+  //   96,
+  //   59,
+  //   181,
+  // ),
+  seedColor: const Color.fromRGBO(255, 118, 117, 1.0),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
