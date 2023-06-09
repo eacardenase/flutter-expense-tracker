aacf0ac8f0c7a99c118abe96bff865ab985906c9
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-09 15:06:36 -0500

    feat: add support to landscape mode

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -91,6 +91,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text(
         'Not expenses found. Start adding some!',
@@ -122,16 +124,29 @@ class _ExpensesState extends State<Expenses> {
         ],
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Chart(
-              expenses: _registeredExpenses,
-            ),
-            Expanded(
-              child: mainContent,
-            )
-          ],
-        ),
+        child: width < 600
+            ? Column(
+                children: [
+                  Chart(
+                    expenses: _registeredExpenses,
+                  ),
+                  Expanded(
+                    child: mainContent,
+                  )
+                ],
+              )
+            : Row(
+                children: [
+                  Expanded(
+                    child: Chart(
+                      expenses: _registeredExpenses,
+                    ),
+                  ),
+                  Expanded(
+                    child: mainContent,
+                  )
+                ],
+              ),
       ),
     );
   }
